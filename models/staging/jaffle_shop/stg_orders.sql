642e189c28e8a4b5fab8a72a687ef5ecb4b78d20
Murilo Biss <murilobiss@gmail.com>
2024-03-22 18:26:45 +0000

    updated sources

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,4 +1,10 @@
-with orders as (
+with source as(
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+orders as (
     
     select
         id as order_id,
@@ -6,7 +12,7 @@ with orders as (
         order_date,
         status
 
-    from dbt-tutorial.jaffle_shop.orders
+    from source
 )
 
 select * from orders
